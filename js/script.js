05ecc82ec96edb7b9248846bd749af0d713bccc4
Anna Powaska <annapowaska@op.pl>
2024-07-28 11:44:03 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-const yo = document.querySelectorAll('.')
+const yo = document.querySelectorAll('list a')
+const lol = document.querySelectorAll('.post')
+
+function argo (event){
+    event.preventDefault();
+    const clickedEl = this;
+    yo.classList.remove('.active')
+    clickedEl.classList.add('.active')
+   const href = clickedEl.getAttribute('href')
+}
+
+
+
+for(let y of yo){
+    y.addEventListener('click', argo)
+}
