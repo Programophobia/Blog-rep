b226cb04d0cab5343e93746e53a6bff37f40ef1a
Anna Powaska <annapowaska@op.pl>
2024-07-28 12:12:57 +0200

    <3

--- js/script.js
+++ js/script.js
@@ -1,16 +1,25 @@
-const yo = document.querySelectorAll('list a')
+
 const lol = document.querySelectorAll('.post')
 
 function argo (event){
+   
     event.preventDefault();
     const clickedEl = this;
-    yo.classList.remove('.active')
-    clickedEl.classList.add('.active')
+    const yoo = document.querySelectorAll('.titles a.active')
+    for(let p of yoo){
+        p.classList.remove('active')
+    }
+    clickedEl.classList.add('active')
    const href = clickedEl.getAttribute('href')
+   for(let mateusz of lol){
+    mateusz.classList.remove('active')
+   }
+   const mateuszek = document.getElementById(href)
+mateuszek.classList.add('active')
 }
 
 
-
+const  yo = document.querySelectorAll('.titles a')
 for(let y of yo){
     y.addEventListener('click', argo)
 }
