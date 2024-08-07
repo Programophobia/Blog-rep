d59845bbe11ea5200b3cafb1f03b0b240f607b2b
Anna Powaska <annapowaska@op.pl>
2024-08-07 04:33:27 +0200

    a

--- js/j.js
+++ js/j.js
@@ -1,9 +1,28 @@
 
 
-function j {
-    
+function j () {
+    const clicked = this
+   const hhh = clicked.getAttribute('href')
+   console.log(hhh)
+ const bb =  document.querySelectorAll('.list.titles')
+    for(let g of bb){
+        g.classList.remove('active')
+    }
+clicked.classList.add('active')
 
+const allart = document.querySelectorAll('.posts')
+
+for(let cv of allart){
+
+    cv.classList.remove('active')
 }
+hh = hhh.replace('#', '')
+const match = document.getElementById(hh)
+
+match.classList.add('active')
+   }
+
+
 
 const h = document.querySelectorAll('.list.titles')
 
