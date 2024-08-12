18772e5c024a116580244c722847e25b384b879f
Anna Powaska <annapowaska@op.pl>
2024-08-12 05:35:37 +0200

    f

--- js/j.js
+++ js/j.js
@@ -106,3 +106,57 @@ ooo('[data-tags~= "'+maluch+'"]')
    
        }
 kawa();
+
+function generateTags(){
+    /* find all articles */
+    const allA = document.querySelectorAll('.')
+    
+    for(let g of all){
+        const wrapper = document.querySelector('.')
+        let h =''
+    }
+       /* find tags wrapper */
+
+    /* make html variable with empty string */
+    /* get tags from data-tags attribute */
+    const tag = wrapper.getAttribute('')
+    /* split tags into array */
+    /* START LOOP: for each tag */
+
+      /* generate HTML of the link */
+      /* add generated code to html variable */
+    /* END LOOP: for each tag */
+    /* insert HTML of all the links into the tags wrapper */
+  /* END LOOP: for every article: */
+ }
+ generateTags();
+
+ function tagClickHandler(event){
+    /* prevent default action for this event */
+    event
+    /* make new constant named "clickedElement" and give it the 
+  value of "this" */
+    /* make a new constant "href" and read the attribute "href" of 
+  the clicked element */
+    /* make a new constant "tag" and extract tag from the "href" 
+  constant */
+    /* find all tag links with class active */
+
+    /* START LOOP: for each active tag link */
+    /* remove class active */
+  /* END LOOP: for each active tag link */
+  /* find all tag links with "href" attribute equal to the "href" 
+constant */
+  /* START LOOP: for each found tag link */
+    /* add class active */
+  /* END LOOP: for each found tag link */
+  /* execute function "generateTitleLinks" with article selector 
+as argument */
+ }
+ function addClickListenersToTags(){
+  /* find all links to tags */
+  /* START LOOP: for each link */
+    /* add tagClickHandler as event listener for that link */
+  /* END LOOP: for each link */
+ }
+ addClickListenersToTags();
