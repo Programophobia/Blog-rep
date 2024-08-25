986ff91204c056a361615a62f41ed24c149bf971
Anna Powaska <annapowaska@op.pl>
2024-08-25 09:31:13 +0200

    ssss

--- f.js
+++ f.js
@@ -1,5 +1,41 @@
 'use strict'
 
+
+
+const ale = document.querySelectorAll('.o')
+
+function click(){
+console.log('clicked')
+const clicked = this
+
+
+for(let a of ale){
+    a.classList.remove('.active')
+}
+clicked.classList.add('.active')
+const gg = document.querySelectorAll('articles')
+for(let g of gg){
+    g.classList.remove('.active')
+
+}
+const href = clicked.getAttribute('href')
+const ladny = href.replace('#-', '')
+console.log(ladny)
+//gg.querySelector(ladny).classList.add('.active')
+//const h = gg.querySelector(ladny)
+  /* find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.getElementById(ladny);
+
+
+  /* add class 'active' to the correct article */
+  targetArticle.classList.add("active");
+
+}
+
+
+
+
+
 function a(){
     const lel = document.querySelectorAll('.post')
     const gdzie = document.querySelector('.list.titles')
@@ -11,31 +47,9 @@ function a(){
             html +=al
             gdzie.innerHTML = html
         }
+        for(let g of ale){
+            g.addEventListener('click', click)
+        }
     }
 
 a()
-
-const ale = document.querySelectorAll('.o')
-
-function click(){
-console.log('clicked')
-const clicked = this
-
-
-for(let a of ale){
-    a.classList.removed('.active')
-}
-clicked.classList.add('.active')
-for(let g of gg){
-    g.classList.remove('.active')
-
-}
-const href = clicked.getAttribute('href')
-const ladny = href.replace('#-', '')
-document.article.querySelector(ladny).classList.add('.active')
-}
-
-
-for(let g of ale){
-    g.addEventListener('click', click)
-}
