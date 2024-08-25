19f3ce1f18c83d31809ea6624b2b95fb5c847605
Anna Powaska <annapowaska@op.pl>
2024-08-25 08:41:47 +0200

    dddd

--- f.js
+++ f.js
@@ -1,10 +1,26 @@
 'use strict'
 
 function a(){
-    const lel = document.querySelectorAll('.post.active')
+    const lel = document.querySelectorAll('.post')
     const gdzie = document.querySelector('.list.titles')
+    let html = ''
         for(let f of lel){
             const nazwa = f.getAttribute('id')
             console.log(nazwa)
+            const al = '<li><a class ="o" href="#-'+nazwa+'"> '+nazwa+'</a></li>'
+            html +=al
+            gdzie.innerHTML = html
         }
+    }
+
+a()
+
+
+function click(){
+console.log('clicked')
+}
+
+const ale = document.querySelectorAll('.o')
+for(let g of o){
+    g.addEventListener('click', click)
 }
